a5183a9bdb49794f1f4071672712b5c4fd601c96
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-11 00:16:13 +0000

    second_solution

--- main.py
+++ main.py
@@ -15,34 +15,51 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-generated_password = []
+# generated_password = []
 
-while nr_letters:
-  random_letter_index = random.randint(0,len(letters)-1)
-  letter = letters[random_letter_index]
-  generated_password.append(letter)
-  nr_letters-=1
-
-
-while nr_symbols:
-  random_symbol_index = random.randint(0,len(symbols)-1)
-  symbol = symbols[random_symbol_index]
-  generated_password.append(symbol)
-  nr_symbols-=1
+# while nr_letters:
+#   random_letter_index = random.randint(0,len(letters)-1)
+#   letter = letters[random_letter_index]
+#   generated_password.append(letter)
+#   nr_letters-=1
 
 
-while nr_numbers:
-  random_number_index = random.randint(0,len(numbers)-1)
-  number = numbers[random_number_index]
-  generated_password.append(number)
-  nr_numbers-=1
+# while nr_symbols:
+#   random_symbol_index = random.randint(0,len(symbols)-1)
+#   symbol = symbols[random_symbol_index]
+#   generated_password.append(symbol)
+#   nr_symbols-=1
 
-new_password =''
-while len(generated_password) > 0:
-  random_element_index = random.randint(0,len(generated_password)-1)
-  el = generated_password.pop(random_element_index)
-  new_password+=el
+
+# while nr_numbers:
+#   random_number_index = random.randint(0,len(numbers)-1)
+#   number = numbers[random_number_index]
+#   generated_password.append(number)
+#   nr_numbers-=1
+
+# new_password =''
+# while len(generated_password) > 0:
+#   random_element_index = random.randint(0,len(generated_password)-1)
+#   el = generated_password.pop(random_element_index)
+#   new_password+=el
   
+password_list=[]
 
+for char in range(1, nr_letters +1 ):
+  password_list +=random.choice(letters)
 
-print(new_password)
+for char in range(1,nr_symbols +1):
+  password_list += random.choice(symbols)
+
+for char in range(1,nr_numbers+1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+password =''
+
+for char in password_list:
+  password += char
+
+print(password)
